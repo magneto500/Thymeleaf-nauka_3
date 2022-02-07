145a1273939cf69128a7b37983a8330d04710bf8
magneto500 <tomasz.piotr.grabowski@gmail.com>
2022-02-07 11:59:22 +0100

    iteration list

--- src/main/java/com/example/thymeleafnauka/CarController.java
+++ src/main/java/com/example/thymeleafnauka/CarController.java
@@ -6,15 +6,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 
 @Controller
 public class CarController {
 				@GetMapping("/car")
 
-				public String get(Model model){
+				public String get(Model model) {
 								model.addAttribute("name", "Tomek");
-        Car car = new Car("BMW", "i8");
-        model.addAttribute("Car", car);
+								Car car = new Car("BMW", "i8");
+								Car car1 = new Car("Opel", "Corsa OPC");
+								Car car2 = new Car("Opel", "Astra");
+								Car car3 = new Car("Suzuki", "SX4 S-Cross");
+								model.addAttribute("Car",car);
+								List<Car> cars = Arrays.asList(car,car1,car2,car3);
+								model.addAttribute("cars",cars);
+
+
 								return "car";
 				}
 }
